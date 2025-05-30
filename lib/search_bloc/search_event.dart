4b53d0e52bfb61c5383438836ae491dec12dd7a5
moham5234 <m0oh.ahmed10@gmail.com>
2025-05-30 16:04:31 -0700

    Delete lib directory

--- lib/search_bloc/search_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'search_bloc.dart';
-
-@immutable
-sealed class SearchEvent {}
-class SearchAppointmentsEvent extends SearchEvent {
-  final String keyword;
-  SearchAppointmentsEvent(this.keyword);
-}
